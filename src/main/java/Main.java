4801b4ce9dab2b44caacff2c815a00ebc0e6bf30
AboZiedA <abdelrahman.abozied@vodafone.com>
2022-09-09 19:15:22 +0200

    Last Commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,12 +2,12 @@ public class Main {
 
     public static void main(String[] args) {
         AnimalSounds dog = new AnimalSounds(new Dog());
-        System.out.println("Dog should say" + dog.howToSpeak());
+        System.out.println("Dog should say " + dog.howToSpeak());
 
         AnimalSounds cat = new AnimalSounds(new Cat());
-        System.out.println("Dog should say" + cat.howToSpeak());
+        System.out.println("cat should say " + cat.howToSpeak());
 
         AnimalSounds bird = new AnimalSounds(new Bird());
-        System.out.println("Dog should say" + bird.howToSpeak());
+        System.out.println("bird should say " + bird.howToSpeak());
     }
 }
